414b3f0f4e8eee5920f13d524d070c51f7165bfd
Sosea1 <kospryshin@gmail.com>
2024-05-19 12:04:11 +0600

    first commit

---
+++ db_image/init.sql
@@ -0,0 +1,19 @@
+DROP DATABASE IF EXISTS ${DB_DATABASE};
+CREATE DATABASE ${DB_DATABASE};
+
+DO $$
+BEGIN
+   IF NOT EXISTS (SELECT FROM pg_catalog.pg_roles WHERE rolname = '${DB_REPL_USER}') THEN
+     CREATE USER ${DB_REPL_USER} REPLICATION LOGIN PASSWORD '${DB_REPL_PASSWORD}';
+   END IF;
+END $$;
+\c ${DB_DATABASE}
+DROP TABLE IF EXISTS emails;
+DROP TABLE IF EXISTS phone_numbers;
+CREATE TABLE  phone_numbers (id SERIAL PRIMARY KEY, phone VARCHAR(30) NOT NULL);
+CREATE TABLE  emails (id SERIAL PRIMARY KEY, email VARCHAR(100) NOT NULL);
+
+INSERT INTO emails (email) VALUES ('test@test.ru');
+INSERT INTO emails (email) VALUES ('test_2@test.ru');
+INSERT INTO phone_numbers (phone) VALUES ('80000000000');
+INSERT INTO phone_numbers (phone) VALUES ('+7(000)000-00-00');
